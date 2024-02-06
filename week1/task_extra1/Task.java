563fdfde87cbdac1e3e504139badb9666416d4cf
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-06 21:07:32 +0500

    clean the code

---
+++ week1/task_extra1/Task.java
@@ -0,0 +1,19 @@
+package task_extra1;
+
+
+class Task {
+
+  static{
+    try {
+    // process.exit(0);
+      throw new Error("er");
+    }catch(Error e){
+       System.out.println(e.getMessage());
+    }
+  }
+  // public static void main(String[] args) {
+  //   System.out.println("Hello World!");
+  // }
+}
+
+ 
