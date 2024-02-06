cbe7e548d57dac03ebe0ed65868617bb8f57f929
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-06 16:09:16 +0500

    test

---
+++ week1/Task.java
@@ -0,0 +1,17 @@
+
+
+class Task {
+
+  static{
+   try {
+      throw new Error("er");
+    }catch(){
+    }
+  }
+  for(var i=0; i<5; i++) {}
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+ 
