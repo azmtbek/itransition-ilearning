64965e0ddd2925ccac45104a950af36ec18bb4a0
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-06 13:32:58 +0500

    finish task 2

--- week1/task2.js
+++ week1/task2.js
@@ -4,7 +4,6 @@ const {
 } = require('node:crypto');
 const path = require('node:path');
 const folderPath = './task2/'
-const ansFolderPath = './task2ans/'
 
 let c = 0
 const arr = []
@@ -18,32 +17,16 @@ fs.readdirSync(folderPath)
       hash.update(stats);
       const ans = hash.digest('hex')
       arr.push(ans)
-      fs.writeFileSync(path.join(ansFolderPath, fileName), ans, { flag: 'w' });
-      console.log(c++)
     } catch (err) {
       console.error(err);
     }
   })
-c=0
-// fs.readdirSync(ansFolderPath)
-//   .map(fileName => {
-//     if (!fs.lstatSync(path.join(ansFolderPath, fileName)).isFile()) return
-//     try {
-//       const stats = fs.readFileSync(path.join(ansFolderPath, fileName));
-//       arr.push(stats.toString('hex'))
-//       console.log(c++)
-//     } catch (err) {
-//       console.error(err);
-//     }
-  // })
-  
+
 try {
   arr.sort((a, b) => a.toString().localeCompare(b.toString()))
-  console.log(arr)
-  fs.writeFileSync('task2sum.data', arr.join(''), { flag: 'w' });
-  const stats = fs.readFileSync('task2sum.data');
+  arr.push("azamatbek.dev@gmail.com")
   const hash = createHash('SHA3-256');
-  hash.update(stats);
+  hash.update(arr.join(''));
   const ans = hash.digest('hex')
   console.log(ans);
   fs.writeFileSync('task2ans.txt', ans, { flag: 'w' });
