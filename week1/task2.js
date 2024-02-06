9c5c38b4d59933964284084f1dc5578b55d017a9
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-06 12:51:39 +0500

    wip: save failed attempt task2

--- week1/task2.js
+++ week1/task2.js
@@ -3,7 +3,6 @@ const {
   createHash,
 } = require('node:crypto');
 const path = require('node:path');
-const hash = createHash('sha256');
 const folderPath = './task2/'
 const ansFolderPath = './task2ans/'
 
@@ -11,12 +10,36 @@ fs.readdirSync(folderPath)
   .map(fileName => {
     try {
       const stats = fs.readFileSync(path.join(folderPath,fileName));
-
+      const hash = createHash('SHA3-256');
       hash.update(stats);
       const ans = hash.digest('hex')
       console.log(ans);
-      fs.writeFileSync(path.join(ansFolderPath,fileName), ans, { flag: 'w+' });
+      fs.writeFileSync(path.join(ansFolderPath,fileName), ans, { flag: 'w' });
     } catch (err) {
       console.error(err);
     }
   })
+const arr = []
+fs.readdirSync(ansFolderPath)
+  .map(fileName => {
+    try {
+      const stats = fs.readFileSync(path.join(ansFolderPath, fileName));
+      arr.push(stats)
+    } catch (err) {
+      console.error(err);
+    }
+  })
+  
+try {
+  arr.sort((a, b) =>a.toString().localeCompare(b.toString()))
+  fs.writeFileSync('task2sum.data', arr.join(''), { flag: 'w' });
+  const stats = fs.readFileSync('task2sum.data');
+  const hash = createHash('SHA3-256');
+  hash.update(stats);
+  const ans = hash.digest('hex')
+  console.log(ans);
+  fs.writeFileSync('task2ans.txt', ans, { flag: 'w' });
+
+} catch (err) {
+  console.error(err);
+}
