2189186c0c9d94e67a81b0ec399143c0b3003ab9
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-07 16:55:09 +0500

    setup task4 front

---
+++ week1/task_4/front/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className=' text-9xl'>Vite + React</h1>
+      <div className="card">
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ week1/task_4/front/src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>Admin panel</div>
+  );
+}
+
+export default Header;
